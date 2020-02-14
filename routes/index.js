8665911a4a1c5ec95bc0e9c9b4556e4c3d1e6c8e
chaos <1414249719@qq.com>
2020-02-14 23:10:00 +0800

    add love

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'chaos' });
+  res.render('index', { title: 'To My Love' });
 });
 
 module.exports = router;
