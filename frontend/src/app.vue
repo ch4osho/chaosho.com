c0adb111f690e08d256409865c0008c5a2f8729d
chaos <1414249719@qq.com>
2020-02-14 23:04:34 +0800

    add love

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,13 +1,16 @@
 <template>
     <div id="chaos">
-        <h1>index</h1>
+        <!-- <h1>index</h1> -->
 
-        <router-link v-for="item in routerList" :to="{name: item.name}" :key="item.name" tag="button">{{item.name}}</router-link>
+        <!-- <router-link v-for="item in routerList" :to="{name: item.name}" :key="item.name" tag="button">{{item.name}}</router-link> -->
 
         <!-- <keep-alive>
             <router-view></router-view>
         </keep-alive> -->
-        <router-view></router-view>
+        <!-- <router-view></router-view> -->
+        <p class="me">我是何超豪</p>
+    <span v-for="item in 520" :key="item">陈佩纯<span v-if="item == 150" class="love">我爱你</span>
+    </span>
     </div>
 </template>
 
@@ -30,5 +33,10 @@ export default {
 </script>
 
 <style>
-
+    .me{
+        margin: 5px 0px
+    }
+    .love{
+        color: red
+    }
 </style>
