e479ad51026fae87853fcdac0743be6faead9f79
chaos <1414249719@qq.com>
2020-03-01 16:14:34 +0800

    add

--- vue-frontend/build/bundle.js
+++ vue-frontend/build/bundle.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 
-const publicPath = "public/Vue-app"
-const tplPath = "public/Vue-app/app.html"
+const publicPath = "public/vue-app"
+const tplPath = "public/vue-app/app.html"
 
 const resolve = (p) => path.resolve(__dirname, "..", p)
 const resolveToPublic = (p) => path.resolve(__dirname, "../..", p)
