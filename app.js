98f1af3f2a2c3d5025b2ff6cfe97b0d4ef33a78f
chaos <1414249719@qq.com>
2020-03-27 11:54:54 +0800

    add chatRoom

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// 以下是websokect依赖
+const ws = require('ws');
+const uuid = require('uuid');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -38,6 +41,79 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+
+// websokect
+
+let socketServer = ws.Server;
+
+let wss = new socketServer({port: 443})
+
+let clients = []
+
+let clientIndex = 0;
+
+function broadcastSend(type,msg,nickname){
+  clients.forEach(function(client,index){
+      if(client.ws.readyState === ws.OPEN) {
+          client.ws.send(JSON.stringify({
+              'type': type,
+              'nickname': nickname,
+              'message': msg,
+              'visitedNum': clientIndex
+          }))
+      }
+  })
+}
+
+wss.on('connection',function(ws){
+  let client_uuid = uuid.v4();
+  let nickname = `陌生人${clientIndex++}`;
+  clients.push({
+      "id": client_uuid,
+      "ws": ws,
+      "nickname": nickname
+  })
+
+  broadcastSend("join", '加入聊天室', nickname);
+
+
+
+  console.log(`client ${client_uuid} connected`);
+
+  function closeSocket(){
+      for(let i = 0;i<clients.length;i++) {
+          if(clients[i].id == client_uuid) {
+              let disconnect_message = `${nickname} 退出群聊`;
+              broadcastSend("notification", disconnect_message, nickname);
+              clients.splice(i, 1);
+          }
+      }
+  }
+
+  ws.on('message', function(message) {
+      if(message.indexOf('/nick') === 0) {
+          let nickname_array = message.split(' ');
+          if(nickname_array.length >= 2) {
+              let old_nickname = nickname;
+              nickname = nickname_array[1];
+              let nickname_message = `游客 ${old_nickname}  改名为 ${nickname}`;
+              console.log(message)
+              broadcastSend("nick_update", nickname_message, nickname);
+          }
+      } else {
+          broadcastSend("message", message, nickname);
+      }
+  })
+
+  ws.on('close', function() {
+      closeSocket();
+  })
+})
+
+// websokect结束
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -50,6 +126,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.listen(80)
+app.listen(8088)
 
 module.exports = app;
